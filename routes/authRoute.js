ec700984d818f002284c3a216386f8bb98c2c39e
Mariam Sharaf <maryamshaider25@gmail.com>
2025-07-29 19:49:31 +0300

    add sign-in && login && logout

---
+++ routes/authRoute.js
@@ -0,0 +1,106 @@
+const router = require('express').Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
+router.get('/signUp', (req, res) => {
+    res.render('auth/signUp.ejs', { error: null })
+})
+
+router.post('/signUp', async (req, res) => {
+    try {
+        const { username, password } = req.body
+
+        if (!username || !password) {
+            return res.render("auth/signUp", {
+                error: "All fields are required."
+            })
+        }
+
+        if (password.length < 6) {
+            return res.render("auth/signUp", {
+                error: "Password must be at least 6 characters long."
+            })
+        } else if (/\s/.test(password)) {
+            return res.render("auth/signUp", {
+                error: "Password contains empty space."
+            })
+        }
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.render("auth/signUp", {
+                error: "Username is already taken."
+
+            })
+        }
+
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        const newUser = {
+            username,
+            password: hashedPassword,
+        }
+
+        const createdUser = await User.create(newUser)
+        req.session.user = {
+            username: createdUser.username
+        }
+        res.redirect("/auth/welcome")
+
+    } catch (error) {
+        console.error("Sign Up error:", error);
+        res.render("auth/signUp", {
+            error: "Something went wrong. Please try again."
+        })
+    }
+})
+
+router.get("/welcome", (req, res) => {
+    if (!req.session.user) {
+        return res.redirect("/auth/login")
+    }
+
+    res.render("auth/welcome.ejs", {
+        username: req.session.user.username
+    })
+})
+
+router.get("/login", (req, res) => {
+    res.render("auth/login.ejs", { error: null })
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username });
+
+        if (!userInDatabase) {
+            return res.render("auth/login", { error: "Username not found." });
+        }
+
+        const validPassword = bcrypt.compareSync(
+            req.body.password,
+            userInDatabase.password
+        )
+
+        if (!validPassword) {
+            return res.render("auth/login", { error: "Incorrect password." });
+        }
+
+        req.session.user = {
+            username: userInDatabase.username,
+            _id: userInDatabase._id
+        }
+
+        res.redirect("/auth/welcome")
+
+    } catch (error) {
+        console.error("Error during sign in:", error)
+        res.render("auth/signUp", { error: "An unexpected error occurred." })
+    }
+})
+
+router.get("/logout", (req, res) => {
+    req.session.destroy()
+    res.redirect("/auth/login")
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,38 @@
-const express = require("express") 
+const express = require("express")
 const app = express()
-const dotenv = require("dotenv").config() 
+const dotenv = require("dotenv").config()
+const morgan = require("morgan")
 const methodOverride = require("method-override")
 const conntectToDB = require('./config/db')
-
+const authRoute = require('./routes/authRoute')
+const listRoute = require('./routes/listRoute')
+const taskRoute = require('./routes/taskRoute')
+const session = require("express-session")
+const passUserToView = require('./middleware/passUserToView')
+const isSignedIn = require("./middleware/isSignedIn")
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride("_method"))
-
+app.use(morgan("dev"))
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+)
+app.use(passUserToView)
+app.set("view engine", "ejs")
 conntectToDB()
 
-
+app.use('/auth', authRoute)
+app.use(isSignedIn)
+// app.use('/lists', listRoute)
+// app.use('/tasks', taskRoute)
 
 const port = process.env.PORT || 3000
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Listening on port " + port)
 })
