81bbb046d66d4e78353d3e7150566ff1c4886d7a
Mariam Sharaf <maryamshaider25@gmail.com>
2025-07-30 12:59:48 +0300

    add update and delete to listRoute

--- routes/listRoute.js
+++ routes/listRoute.js
@@ -1,4 +1,5 @@
 const List = require('../models/List')
+const Task = require('../models/Task')
 const router = require('express').Router()
 
 router.get('/new', (req, res) => {
@@ -21,6 +22,34 @@ router.get('/', async (req, res) => {
         res.render('list/allLists.ejs', { allLists: allLists })
     } catch (error) {
         console.log(error)
+    }
+})
+
+router.get('/edit/:id', async (req, res) => {
+    try {
+        const foundList = await List.findById(req.params.id)
+        res.render('list/updateList.ejs', { foundList: foundList})
+    }
+    catch (error) {
+        console.log(error)
+    }
+})
+
+router.put('/edit/:id', async (req, res) => {
+    try {
+        const updatedList = await List.findByIdAndUpdate(req.params.id, req.body)
+        res.redirect('/lists')
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const deletedList = await List.findByIdAndDelete(req.params.id)
+        res.redirect('/lists')
+    } catch (e) {
+        console.log(e)
     }
 })
 
